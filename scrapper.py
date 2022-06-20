89d9d8d64629f6f8bb838ff9113973314c28cc00
Nirman529 <68377229+Nirman529@users.noreply.github.com>
2022-06-20 16:58:58 +0530

    scrap for iframe then network

--- scrapper.py
+++ scrapper.py
@@ -1,18 +1,32 @@
 from seleniumwire import webdriver
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 import requests
 import json
 import time
-import sys
 import os
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 
-def main(argv):
+def main():
 
-    print(argv)
+    argv = input("Enter url: ")
+    turl = argv
+    html = urlopen(turl).read()
+    soup = BeautifulSoup(html, "html.parser")
+    tags = soup.find('iframe')
+    gotourl = tags.get('src')
+
+    # print(gotourl)
     # Header required for receiving the file in the localhost
     headers = {
         'X-Requested-With': 'XMLHttpRequest',
         'content-type': 'application/octet-stream'
+        # 'content-type': 'text/html'
     }
 
     # Create a new instance of the Chrome driver
@@ -24,7 +38,7 @@ def main(argv):
     # pass (argv --> video link) in the get method
 
     driver.get(
-        argv[0]
+        gotourl
         # 'https://v3601506.v360.in/vision360.html?d=11914-516259491&z=1&surl=https%3a%2f%2fv3601506.v360.in%2f'
     )
 
@@ -32,7 +46,7 @@ def main(argv):
     # Access requests via the `requests` attribute
     count = 0
     urls = []
-    folder=''
+    folder = ''
 
     for request in driver.requests:
         count = count + 1
@@ -40,32 +54,34 @@ def main(argv):
         if request.response:
             # ----- startswith will fetch all the files with the file name and store them to urls
             if request.url.startswith("https://v3601506.v360.in/imaged/"):
+
                 getUrl = request.url
-                print("url : ", getUrl)
+                # print("url : ", getUrl)
                 urls.append(getUrl)
 
                 start = 'https://v3601506.v360.in/imaged/'
                 end = '/'
                 folder = (getUrl.split(start))[1].split(end)[0]
-                print(folder)
-                
-                
+                # print(folder)
+
                 # folder = 'tempDir'
+                # folder = "../"+folder
                 try:
                     # Create target Directory
                     os.mkdir(folder)
-                    print("Directory " , folder ,  " Created ") 
+                    # print("Directory ", folder,  " Created ")
                 except FileExistsError:
-                    print("Directory " , folder ,  " already exists")
+                    print()
+                    # print("Directory ", folder,  " already exists")
 
-    print("Total requests made:", count)
-    print("Urls extracted:", urls)
+                    # print("Total requests made:", count)
+                    # print("Urls extracted:", urls)
 
     count = 0
 
     # Check in individual url for the contents of the json file
+    json_result = []
     for url in urls:
-        json_result = []
         # Create file name for image file
         # path = 'C:\\Users\\Username\\Path\\To\\File'
 
@@ -77,20 +93,22 @@ def main(argv):
             json_result.append((requests.get(url, headers=headers)).json())
 
             # convert the json response to string to paste it to the file
-            fileContent = json.dumps(json_result[0])
+            fileContent = json.dumps(json_result[count])
 
             # write the content of res[i] to the file
             f.write(fileContent)
-            print('Saving content to ', fileName)
+            # print('Saving content to ', fileName)
 
             # close the file
-            print('length of file: ', len(json_result[0]))
+            # print('length of file: ', len(json_result[count]))
 
+        f.close()
         count = count + 1
 
     # After the program execution is complete the driver is cleared
     del driver.requests
+    print("Done execution")
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
